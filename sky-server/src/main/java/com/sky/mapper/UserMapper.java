f21ea980221f401dfc730bbff8cb127459825bcc
Maverick-dunk <lgxxlfy@163.com>
2024-02-20 02:35:21 +0800

    微信小程序登录功能开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+@Mapper
+public interface UserMapper {
+
+    void insert(User user);
+
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid(String openid);
+}
